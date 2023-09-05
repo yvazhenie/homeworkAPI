3468b0711f5f63a8b3af2276e2d5f391d75ced94
Makarov <infernochetac31@gmail.com>
2023-09-06 02:07:32 +0300

    start hw

---
+++ src/main/java/makarov/homeworkApi/HomeworkApiApplication.java
@@ -0,0 +1,13 @@
+package makarov.homeworkApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/makarov/homeworkApi/HomeworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package makarov.homeworkApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
